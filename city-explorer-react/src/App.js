7f03ae58f0ad0bb250b5ee456f9a1726c321f85a
surfwalker <alexanderkreid@gmail.com>
2019-07-24 11:41:22 -0700

    Added some scaffolding.

--- city-explorer-react/src/App.js
+++ city-explorer-react/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from './header.js';
+import SearchForm from './search-form.js';
+import Map from './map.js';
+import SearchResults from './search-results.js';
 
 function App() {
-  return ();
+  return (
+    <>
+      <Header />
+      <SearchForm />
+      <Map />
+      <SearchResults />
+    </>
+  );
 }
 
 export default App;
